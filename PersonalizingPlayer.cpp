c0c4b315fdd0dc0811bf505e5d8c3cd4be4b3550
Prateek <prateektomar005@gmail.com>
2024-08-27 00:11:12 +0530

    PlayerPersonalization@Outscal

---
+++ PersonalizingPlayer.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+
+class Player
+{
+    private:
+    int health;
+    int healingPower;
+    int attackingPower;
+
+    public:
+    Player()
+    {
+        cout<<"----Menco - One Last Fight!----"<<endl;
+        cout<<"After countless fights and titles. Menco is now going to say bye to UFC and MMA forever" <<endl;
+        cout<<"But But... as we say every legend deserves a legendary goodbye so here we have One last Fight "<<endl;
+        cout<< "----Menvo vs Draco---"<<endl;
+    }
+};
+
+int main()
+{
+ Player playerobj;
+  }
